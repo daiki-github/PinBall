37428eda3f33097c4aa2ad64c5366c97ae05d35f
古林大輝 <daiki-h9.2.14@i.softbank.jp>
2018-10-26 14:56:08 +0900

    PinBallの作品です。

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -10,10 +10,18 @@ public class BallController : MonoBehaviour {
 	//ゲームオーバを表示するテキスト
 	private GameObject gameoverText;
 
+	//スコアを表示するテキスト（追加）
+	private GameObject scoreText;
+	//得点（追加）
+	private int score = 0;
+
 	// Use this for initialization
 	void Start () {
 		//シーン中のGameOverTextオブジェクトを取得
 		this.gameoverText = GameObject.Find("GameOverText");
+
+		//シーン中のscoreTextオブジェクトを取得（追加）
+		this.scoreText = GameObject.Find("ScoreText");
 	}
 
 	// Update is called once per frame
@@ -24,4 +32,36 @@ public class BallController : MonoBehaviour {
 			this.gameoverText.GetComponent<Text> ().text = "Game Over";
 		}
 	}
+
+	void OnTriggerEnter(Collider other) {
+		//コインに衝突した場合
+		if (other.gameObject.tag =="SmallCloudTag" || other.gameObject.tag =="LargeStarTag") {
+
+			// スコアを加算(追加)
+			this.score += 10;
+
+			//ScoreText獲得した点数を表示(追加)
+			this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+		}
+
+		//コインに衝突した場合
+		if (other.gameObject.tag =="LargeCloudTag") {
+
+			// スコアを加算(追加)
+			this.score += 20;
+
+			//ScoreText獲得した点数を表示(追加)
+			this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+		}
+
+	}
 }
+
+
+
+
+
+
+
+
+
